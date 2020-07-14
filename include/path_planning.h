327e0e55219d4f9937bf7635bb8a3370f7d2d24b
yiluo <yi.luo@in-tech.com>
2020-07-14 18:48:08 +0200

    add path_planning algorithm

---
+++ include/path_planning.h
@@ -0,0 +1,24 @@
+#ifndef PATH_PLANNING_H
+#define PATH_PLANNING_H
+
+#include <map.h>
+#include <math.h>
+
+typedef std::pair<int, int> Point2D;
+struct Point2DWithFloat {
+  Point2D point;
+  float value;
+};
+
+std::vector<Point2D> GetNeighbors(Point2D current, Map map);
+
+float CalculateCostSoFar(Point2D start_pos, Point2D current);
+
+float HeuristicFunction(Point2D next, Point2D end_pos);
+
+void OptimizePath(std::vector<Point2D> &path, Point2D start_pos);
+
+std::vector<Point2D> PathPlanning(Point2D start_pos, Point2D end_pos,
+                                  const Map &map);
+
+#endif  // !PATH_PLANNING_H
