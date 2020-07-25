98b1f2eb0a2a3647af1f01e5f9b2aabfde843b29
Xiangfei <xiangfei_yu@outlook.com>
2020-07-25 17:56:23 +0200

    safety save for scan and odom

--- src/mapbufo/main/main.cpp
+++ src/mapbufo/main/main.cpp
@@ -1,9 +1,15 @@
+#include <geometry_msgs/Point32.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/time_synchronizer.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/package.h>
+#include <ros/rate.h>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
 #include <tf/transform_datatypes.h>
 #include <iostream>
 #include <iterator>
@@ -16,7 +22,12 @@
 #include "robot.h"
 
 static std::vector<Point2D> curr_scan_;
-static Point2D curr_robot_pos_;
+static bool reached_pos_ = true;
+static std::pair<float, float> curr_robot_pos_;
+static std::pair<float, float> estimated_pos_;
+static std::pair<float, float> goal_;
+static bool angle_setted = false;
+
 class CommunicationInterface
 {
 public:
@@ -24,27 +35,147 @@ public:
   {
     pos_publisher_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
     control_publisher_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+    scan_publisher_ = nh_.advertise<sensor_msgs::PointCloud>("/processed_scan", 1);
 
-    scan_subscriber_ = nh_.subscribe("/scan", 1000, &CommunicationInterface::laserScanCallback, this);
+    scan_subscriber_ = nh_.subscribe("/scan", 1, &CommunicationInterface::laserScanCallback, this);
+    pos_subscriber_ = nh_.subscribe("/odom", 1, &CommunicationInterface::robotPositionCallback, this);
 
-    pos_subscriber_ = nh_.subscribe("/odom", 1000, &CommunicationInterface::robotPositionCallback, this);
+    // message_filters::Subscriber<sensor_msgs::LaserScan> scan_input(nh_, "/scan", 3);
+
+    // message_filters::Subscriber<nav_msgs::Odometry> odom_input(nh_, "/odom", 3);
+
+    // message_filters::TimeSynchronizer<sensor_msgs::LaserScan, nav_msgs::Odometry> sync(scan_input, odom_input, 3);
+    // sync.registerCallback(boost::bind(&this->scanOdomCallBack, _1, _2));
   }
 
   void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr &scan)
   {
-  // ROS_INFO("scan received");
+    float angle_min = scan->angle_min;
+    float angle_max = scan->angle_max;
+    float angle_increment = scan->angle_increment;
+    float range_min = scan->range_min;
+    float range_max = scan->range_max;
+    int number_of_laser = scan->ranges.size();
+    // get laser scan id of the mid +- 10 deg
+    float priority_angle_range_deg = 10;
+    float priority_angle_range_rad = priority_angle_range_deg * M_PI / 180.0;
+    // laser scan: 0-----1*****2*****3-----4
+    // 0: angle min
+    // 1: priority start
+    // 2: center
+    // 3: priority end
+    // 4: angle max
 
-  laserscanner:
-    1. filter(20) > 20 var : resolution 1 : 10
+    // process scan
+    sensor_msgs::PointCloud point_cloud;
+    static int frame_id_ = 0;
+    point_cloud.header.frame_id = "/base_footprint";
+    point_cloud.header.stamp = ros::Time::now();
+    point_cloud.header.seq = frame_id_;
+    frame_id_++;
+
+    std::pair<float, float> next_pos(0, 0);
+    float max_dist = 0;
+
+    curr_scan_.clear();
+    int counter = 0;
+    int resolution = 10;  // pick only every ten points outside the interested area
+    for (int i = 0; i < number_of_laser; i++)
+    {
+      float laser_angle = angle_min + i * angle_increment;
+
+      if (laser_angle > -priority_angle_range_rad && laser_angle < priority_angle_range_rad)
+      {
+        if (std::isnan(scan->ranges[i]))  // if a point is nan, then the 10 points before and after must be nan so that
+                                          // it can be considered empty
+        {
+          // check the +- 10 points
+          if ((priority_angle_range_rad - abs(laser_angle)) / angle_increment < 10)
+          {
+            continue;
+          }
+          else
+          {
+            bool valid = true;
+            for (int j = 0; j < 10; j++)
+            {
+              if (!std::isnan(scan->ranges[i + j]) || !std::isnan(scan->ranges[i - j]))
+              {
+                valid = false;
+                break;
+              }
+            }
+            if (!valid)
+            {
+              continue;
+            }
+          }
+          if (range_max > max_dist)
+          {
+            next_pos.first = range_max * std::cos(laser_angle);
+            next_pos.second = range_max * std::sin(laser_angle);
+            max_dist = scan->range_max;
+          }
+          continue;
+        }
+
+        // robot heading: x axis, robot left: y axis
+        float x = scan->ranges[i] * std::cos(laser_angle);
+        float y = scan->ranges[i] * std::sin(laser_angle);
+        // insert
+        curr_scan_.push_back(Point2D(int(x), int(y)));
+        geometry_msgs::Point32 pt;
+        pt.x = x;
+        pt.y = y;
+        pt.z = 0;
+
+        point_cloud.points.push_back(pt);
+        if (scan->ranges[i] > max_dist)
+        {
+          next_pos.first = x;
+          next_pos.second = y;
+          max_dist = scan->ranges[i];
+        }
+      }
+      else
+      {
+        counter++;
+        if (counter < resolution)
+        {
+          continue;
+        }
+        counter = 0;
+        // insert
+        if (std::isnan(scan->ranges[i]))
+          continue;
+        // robot heading: x axis, robot left: y axis
+        float x = scan->ranges[i] * std::cos(laser_angle);
+        float y = scan->ranges[i] * std::sin(laser_angle);
+
+        curr_scan_.push_back(Point2D(int(x), int(y)));
+        geometry_msgs::Point32 pt;
+        pt.x = x;
+        pt.y = y;
+        pt.z = 0;
+
+        point_cloud.points.push_back(pt);
+      }
+    }
+    if (reached_pos_)
+    {
+      estimated_pos_ = next_pos;
+      std::cerr << "scan received" << std::endl;
+      std::cerr << curr_robot_pos_.first << " " << curr_robot_pos_.second << " " << estimated_pos_.first << " "
+                << estimated_pos_.second << std::endl;
+    }
+    scan_publisher_.publish(point_cloud);
   }
 
   void robotPositionCallback(const nav_msgs::Odometry msg)
   {
     // ROS_INFO("pos received");
-    PIDController pid_angle(0.5, 0.01, 2);
-    PIDController pid_speed(0.2, 0.01, 0.0);
-    double goal_x = 10.0;
-    double goal_y = 10.0;
+    PIDController pid_angle(1, 0.2, 2.5);
+    PIDController pid_speed(0.1, 0.00, 0.0);
 
     geometry_msgs::Pose pose;
     geometry_msgs::Twist twist;
@@ -52,37 +183,61 @@ public:
     twist = msg.twist.twist;
     double pos_x = pose.position.x;
     double pos_y = pose.position.y;
-
-    curr_robot_pos_.first = pos_x;
-    curr_robot_pos_.second = pos_y;
-
-    // std::cerr << speed << std::endl;
-    double target_angle = atan2(goal_y - pos_y, goal_x - pos_x);
     tf::Quaternion q(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w);
     tf::Matrix3x3 m(q);
     double roll, pitch, yaw;
     m.getRPY(roll, pitch, yaw);
 
     double robot_heading = yaw;
+    // transform estimated_pos_ into absolute pos
+    if (reached_pos_)
+    {
+      goal_.first = estimated_pos_.first * std::cos(yaw) - estimated_pos_.second * std::sin(yaw) + pos_x;
+      goal_.second = estimated_pos_.first * std::sin(yaw) + estimated_pos_.second * std::cos(yaw) + pos_y;
+    }
+    // goal_.first = 10;
+    // goal_.second = 10;
+    curr_robot_pos_.first = pos_x;
+    curr_robot_pos_.second = pos_y;
+
+    double target_angle = atan2(goal_.second - pos_y, goal_.first - pos_x);
 
     double diff_angle = target_angle - robot_heading;
-
     double update_angle = pid_angle.Control(diff_angle);
+    if (fabs(diff_angle) < 1 * M_PI / 180.0)
+    {
+      if (!angle_setted)
+      {
+        angle_setted = true;
+      }
+    }
+    if (fabs(diff_angle) < 5 * M_PI / 180.0)
+    {
+      if (angle_setted)
+      {
+        update_angle = 0;
+      }
+    }
+    else
+    {
+      angle_setted = false;
+    }
 
-    double diff_x = goal_x - pos_x;
-    double diff_y = goal_y - pos_y;
+    double diff_x = goal_.first - pos_x;
+    double diff_y = goal_.second - pos_y;
     double diff_dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2));
 
     double speed = pid_speed.Control(diff_dist);
 
     double set_angle;
+
     if (set_angle > 0)
     {
-      set_angle = std::min(update_angle, 1.0);
+      set_angle = std::min(update_angle, 0.1);
     }
     else
     {
-      set_angle = std::max(update_angle, -1.0);
+      set_angle = std::max(update_angle, -0.1);
     }
 
     double set_speed;
@@ -94,10 +249,10 @@ public:
     {
       set_speed = std::max(speed, -0.1);
     }
-    std::cout << target_angle * 180.0 / M_PI << "    " << robot_heading * 180.0 / M_PI << "    "
-              << diff_angle * 180.0 / M_PI << "    " << set_angle * 180 / M_PI << std::endl;
+    std::cerr << set_angle << std::endl;
+
     geometry_msgs::Twist new_twist;
-    if (fabs(set_angle) < 1 * M_PI / 180)
+    if (angle_setted)
     {
       new_twist.linear.x = set_speed;
     }
@@ -109,38 +264,68 @@ public:
     new_twist.linear.z = 0.0;
     new_twist.angular.x = 0.0;
     new_twist.angular.y = 0.0;
-    // new_twist.angular.z = 0.0;
     new_twist.angular.z = set_angle;
 
-    control_publisher_.publish(new_twist);
+    if (fabs(curr_robot_pos_.first - goal_.first) > 1e-1 || fabs(curr_robot_pos_.first - goal_.first) > 1e-1)
+    {
+      std::cerr << "not reached yet" << std::endl;
+      std::cerr << "curr pos: " << curr_robot_pos_.first << " " << curr_robot_pos_.second << " "
+                << "next pos: " << goal_.first << " " << goal_.second << std::endl;
+      reached_pos_ = false;
+      control_publisher_.publish(new_twist);
+    }
+    else
+    {
+      std::cerr << "arrived" << std::endl;
+      std::cerr << "curr pos: " << curr_robot_pos_.first << " " << curr_robot_pos_.second << " "
+                << "next pos: " << goal_.first << " " << goal_.second << std::endl;
+      reached_pos_ = true;
+
+      geometry_msgs::Twist stand_still;
+
+      stand_still.linear.x = 0;
+
+      stand_still.linear.y = 0.0;
+      stand_still.linear.z = 0.0;
+      stand_still.angular.x = 0.0;
+      stand_still.angular.y = 0.0;
+      stand_still.angular.z = 0;
+      control_publisher_.publish(stand_still);
+    }
+
+    // std::cerr << goal_.first << " " << goal_.second << std::endl;
   }
 
 private:
   ros::NodeHandle nh_;
   ros::Publisher pos_publisher_;
   ros::Publisher control_publisher_;
+  ros::Publisher scan_publisher_;
   ros::Subscriber scan_subscriber_;
   ros::Subscriber pos_subscriber_;
 };
 
 int main(int argc, char **argv)
 {
-  Map global_map;
+  Map global_map();
   ros::init(argc, argv, "main");
 
   // subscriber for scan and publisher for next pos
   CommunicationInterface CIObject;
 
-  ros::rate loop_rate(10);
+  ros::Rate loop_rate(1);
 
   while (ros::ok())
   {
-    for (auto point : curr_scan_)
-    {
-      global_map.UpdateWithScanpoint(curr_ro, float y0, float x1, float y1, int value);
-    }
+    CIObject.control_publisher_.publish();
+    // for (auto point : curr_scan_)
+    // {
+    //   std::cerr << point.first << " " << point.second << std::endl;
+    // }
+    // std::cerr << curr_robot_pos_.first << " " << curr_robot_pos_.second << std::endl;
+
     ros::spinOnce();
   }
-
+  // ros::spin();
   return 0;
 }
--- src/mapbufo/src/robot.cpp
+++ src/mapbufo/src/robot.cpp
@@ -3,16 +3,19 @@
 // find the best position based on the input scan
 /// 1. if only interested in empty points, then only empty points are considered
 /// 2. if interested in the overall best point, all points are then considered
-void Robot::findBestPos(ScanData scan, Point2D candidate_pt, Point2D &best_pos,
-                        float &best_heading, bool only_empty) {
+void Robot::findBestPos(ScanData scan, Point2D candidate_pt, Point2D &best_pos, float &best_heading, bool only_empty)
+{
   bool init = false;
   float best_heading_diff = 10e5;
   int pt_x = candidate_pt.first;
   int pt_y = candidate_pt.second;
   std::set<CellOccupied> interested_status_list;
-  if (only_empty) {
+  if (only_empty)
+  {
     interested_status_list.insert(CellOccupied::empty);
-  } else {
+  }
+  else
+  {
     interested_status_list.insert(CellOccupied::empty);
     interested_status_list.insert(CellOccupied::path);
     interested_status_list.insert(CellOccupied::unknown);
@@ -21,34 +24,38 @@ void Robot::findBestPos(ScanData scan, Point2D candidate_pt, Point2D &best_pos,
     interested_status_list.insert(CellOccupied::robot_pos);
   }
 
-  if (scan.find({pt_x, pt_y}) != scan.end()) {
+  if (scan.find({ pt_x, pt_y }) != scan.end())
+  {
     // if this point is not occupied
-    if (interested_status_list.find(scan[candidate_pt]) !=
-        interested_status_list.end()) {
+    if (interested_status_list.find(scan[candidate_pt]) != interested_status_list.end())
+    {
       float cur_heading = std::atan2(pt_y, pt_x);
       float cur_heading_diff = std::abs(cur_heading - heading_);
-      if (!init) {
+      if (!init)
+      {
         best_heading = cur_heading;
         best_heading_diff = cur_heading_diff;
         best_pos.first = pt_x;
         best_pos.second = pt_y;
         init = true;
-      } else if (cur_heading_diff < best_heading_diff) {  // choose the one with
-                                                          // smaller angle
-                                                          // difference
+      }
+      else if (cur_heading_diff < best_heading_diff)
+      {  // choose the one with
+         // smaller angle
+         // difference
         best_heading = cur_heading;
         best_heading_diff = cur_heading_diff;
         best_pos.first = pt_x;
         best_pos.second = pt_y;
-      } else if (cur_heading_diff ==
-                 best_heading_diff)  // choose the farther point
-                                     // when angle differences
-                                     // are the same
+      }
+      else if (cur_heading_diff == best_heading_diff)  // choose the farther point
+                                                       // when angle differences
+                                                       // are the same
       {
-        float best_dist =
-            std::sqrt(pow(best_pos.first, 2) + pow(best_pos.second, 2));
+        float best_dist = std::sqrt(pow(best_pos.first, 2) + pow(best_pos.second, 2));
         float cur_dist = std::sqrt(pow(pt_x, 2) + pow(pt_y, 2));
-        if (cur_dist > best_dist) {
+        if (cur_dist > best_dist)
+        {
           best_heading = cur_heading;
           best_heading_diff = cur_heading_diff;
           best_pos.first = pt_x;
@@ -82,17 +89,13 @@ void Robot::findBestPos(ScanData scan, Point2D candidate_pt, Point2D &best_pos,
  * #: --> here in this function, this point is selected as the next move.
  */
 
-Point2D Robot::estimateNextStep(ScanData scan, Point2D end_pos, float max_dist,
-                                bool &call_help) {
-  //  for (auto sc : scan) {
-  //    std::cerr << sc.first.first << " " << sc.first.second << " " <<
-  //    sc.second
-  //              << std::endl;
-  //  }
-
+Point2D Robot::estimateNextStep(ScanData scan, Point2D end_pos, float max_dist, bool &call_help)
+{
   // first check if the end pos is in reach
-  if (scan.find(end_pos) != scan.end()) {
-    if (scan[end_pos] == CellOccupied::empty) return end_pos;
+  if (scan.find(end_pos) != scan.end())
+  {
+    if (scan[end_pos] == CellOccupied::empty)
+      return end_pos;
   }
 
   // if already at the best heading:
@@ -106,9 +109,12 @@ Point2D Robot::estimateNextStep(ScanData scan, Point2D end_pos, float max_dist,
 
   // find points near the line with ratio ~= heading
   float ratio = 0.0;
-  if (std::abs(std::abs(heading_) - M_PI / 2.0) < 10e-2) {
+  if (std::abs(std::abs(heading_) - M_PI / 2.0) < 10e-2)
+  {
     ratio = 0;
-  } else {
+  }
+  else
+  {
     ratio = tan(heading_);
   }
 
@@ -121,17 +127,21 @@ Point2D Robot::estimateNextStep(ScanData scan, Point2D end_pos, float max_dist,
   // 1. x direction: for each x on grid, there's a y value on the line. Get this
   // y value!
   {
-    for (int i = 1; i <= num_steps; i++) {
+    for (int i = 1; i <= num_steps; i++)
+    {
       float y;
       float x;
-      if (std::abs(heading_) - M_PI / 2.0 < -10e-2) {
+      if (std::abs(heading_) - M_PI / 2.0 < -10e-2)
+      {
         x = float(x_ + i);
         y = (x - x_) * ratio + float(y_);
-
-      } else if (std::abs(heading_) - M_PI / 2.0 > 10e-2) {
+      }
+      else if (std::abs(heading_) - M_PI / 2.0 > 10e-2)
+      {
         x = float(x_ - i);
         y = (x - x_) * ratio + float(y_);
-      } else  // heading == +- M_PI
+      }
+      else  // heading == +- M_PI
       {
         x = x_;
         if (heading_ > 0)
@@ -139,70 +149,78 @@ Point2D Robot::estimateNextStep(ScanData scan, Point2D end_pos, float max_dist,
         else
           y = float(y_ - i);
       }
-      line_point_vec.insert({x, floor(y)});
-      line_point_vec.insert({x, ceil(y)});
+      line_point_vec.insert({ x, floor(y) });
+      line_point_vec.insert({ x, ceil(y) });
     }
 
     // 2. y direction: for each y on grid, there's an x value on the line. Get
     // this x value!
 
     {
-      for (int i = 1; i <= num_steps; i++) {
+      for (int i = 1; i <= num_steps; i++)
+      {
         float y;
         float x;
-        if (heading_ > 10e-2 &&
-            heading_ - M_PI / 2.0 < -10e-2) {  // 1st quadrant
+        if (heading_ > 10e-2 && heading_ - M_PI / 2.0 < -10e-2)
+        {  // 1st quadrant
           y = float(y_ + i);
           x = (y - y_) / ratio + float(x_);
-          line_point_vec.insert({floor(x), y});
-          line_point_vec.insert({ceil(x), y});
-        } else if (heading_ < -10e-2 &&
-                   heading_ + M_PI / 2.0 < -10e-2) {  // 4th quadrant
+          line_point_vec.insert({ floor(x), y });
+          line_point_vec.insert({ ceil(x), y });
+        }
+        else if (heading_ < -10e-2 && heading_ + M_PI / 2.0 < -10e-2)
+        {  // 4th quadrant
           y = float(y_ - i);
           x = (y - y_) / ratio + float(x_);
-          line_point_vec.insert({floor(x), y});
-          line_point_vec.insert({ceil(x), y});
-        } else if (M_PI - heading_ > 10e-2 &&
-                   heading_ - M_PI / 2.0 > 10e-2) {  // 2nd quadrant
+          line_point_vec.insert({ floor(x), y });
+          line_point_vec.insert({ ceil(x), y });
+        }
+        else if (M_PI - heading_ > 10e-2 && heading_ - M_PI / 2.0 > 10e-2)
+        {  // 2nd quadrant
           y = float(y_ + i);
           x = (y - y_) / ratio + float(x_);
 
-          line_point_vec.insert({floor(x), y});
-          line_point_vec.insert({ceil(x), y});
-        } else if (M_PI + heading_ > 10e-2 && heading_ + M_PI / 2.0 < -10e-2) {
+          line_point_vec.insert({ floor(x), y });
+          line_point_vec.insert({ ceil(x), y });
+        }
+        else if (M_PI + heading_ > 10e-2 && heading_ + M_PI / 2.0 < -10e-2)
+        {
           y = float(y_ - i);
           x = (y - y_) / ratio + float(x_);
 
-          line_point_vec.insert({floor(x), y});
-          line_point_vec.insert({ceil(x), y});
-
-        } else if (std::abs(std::abs(heading_) - M_PI / 2.0) <
-                   10e-2)  // heading == +- M_PI
+          line_point_vec.insert({ floor(x), y });
+          line_point_vec.insert({ ceil(x), y });
+        }
+        else if (std::abs(std::abs(heading_) - M_PI / 2.0) < 10e-2)  // heading == +- M_PI
         {
           x = x_;
           if (heading_ > 0)
             y = float(y_ + i);
           else
             y = float(y_ - i);
-          line_point_vec.insert({x, floor(y)});
-          line_point_vec.insert({x, ceil(y)});
-        } else  // heading == 0 / M_PI
+          line_point_vec.insert({ x, floor(y) });
+          line_point_vec.insert({ x, ceil(y) });
+        }
+        else  // heading == 0 / M_PI
         {
           y = y_;
-          if (std::abs(heading_) < 10e-2) {
+          if (std::abs(heading_) < 10e-2)
+          {
             x = float(x_ + i);
-
-          } else {
+          }
+          else
+          {
             x = float(x_ - i);
           }
-          line_point_vec.insert({floor(x), y});
-          line_point_vec.insert({ceil(x), y});
+          line_point_vec.insert({ floor(x), y });
+          line_point_vec.insert({ ceil(x), y });
         }
       }
     }
 
     // scan point exists:
-    for (auto pt : line_point_vec) {
+    for (auto pt : line_point_vec)
+    {
       //      int pt_x = pt.first;
       //      int pt_y = pt.second;
       //      if (pt_x < 0 || pt_y < 0) continue;
@@ -211,20 +229,22 @@ Point2D Robot::estimateNextStep(ScanData scan, Point2D end_pos, float max_dist,
     }
   }
 
-  if (best_pos.first == -100 && best_pos.second == -100) {
+  if (best_pos.first == -100 && best_pos.second == -100)
+  {
     best_pos.first = x_;
     best_pos.second = y_;
     best_heading = heading_;
   }
-  if (best_pos.first != farthest_pos.first ||
-      best_pos.second != farthest_pos.second) {
+  if (best_pos.first != farthest_pos.first || best_pos.second != farthest_pos.second)
+  {
     call_help = true;
   }
 
   return best_pos;
 }
 
-void Robot::move(std::pair<int, int> next_pos) {
+void Robot::move(std::pair<int, int> next_pos)
+{
   int dist_x = next_pos.first - x_;
   int dist_y = next_pos.second - y_;
   heading_ = std::atan2(dist_y * 1.0, dist_x * 1.0);
