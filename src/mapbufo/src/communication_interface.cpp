baba129fa88195c8e4c7fb52f5b30fa31c18a2d3
Xiangfei <xiangfei_yu@outlook.com>
2020-07-29 21:25:02 +0200

    functions in communication interface class separated

--- src/mapbufo/src/communication_interface.cpp
+++ src/mapbufo/src/communication_interface.cpp
@@ -6,10 +6,8 @@ CommunicationInterface::CommunicationInterface(ros::NodeHandle &nh) : sync(scan_
   scan_sub.subscribe(nh, "/scan", 10);
   odom_sub.subscribe(nh, "/odom", 10);
   sync.registerCallback(boost::bind(&CommunicationInterface::scanOdomCallback, this, _1, _2));
-  // sync.registerCallback(&CommunicationInterface::scanOdomCallback, this);
   pos_subscriber_ = nh.subscribe("/odom", 1, &CommunicationInterface::robotPositionCallback, this);
-  // output: robot_next_pos, robot_control
-  // pos_publisher_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+  // output: robot_control
   control_publisher_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
 
   pub_map_quadrant_1_ = nh.advertise<nav_msgs::OccupancyGrid>("occu_map_quadrant_1", 100);
@@ -21,14 +19,42 @@ CommunicationInterface::CommunicationInterface(ros::NodeHandle &nh) : sync(scan_
   scan_publisher_ = nh.advertise<sensor_msgs::PointCloud>("/processed_scan", 1);
 }
 void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::ConstPtr &scan,
-                                              const nav_msgs::Odometry::ConstPtr &msg)
+                                              const nav_msgs::Odometry::ConstPtr &odom)
 {
-  float angle_min = scan->angle_min;
-  float angle_max = scan->angle_max;
-  float angle_increment = scan->angle_increment;
-  float range_min = scan->range_min;
-  float range_max = scan->range_max;
-  int number_of_laser = scan->ranges.size();
+  // update input scan
+  input_scan_.header = scan->header;
+  input_scan_.angle_min = scan->angle_min;
+  input_scan_.angle_max = scan->angle_max;
+  input_scan_.angle_increment = scan->angle_increment;
+  input_scan_.time_increment = scan->time_increment;
+  input_scan_.scan_time = scan->scan_time;
+  input_scan_.range_min = scan->range_min;
+  input_scan_.range_max = scan->range_max;
+  input_scan_.ranges.clear();
+  for (auto r : scan->ranges)
+  {
+    input_scan_.ranges.push_back(r);
+  }
+  input_scan_.intensities.clear();
+  for (auto i : scan->intensities)
+  {
+    input_scan_.intensities.push_back(i);
+  }
+
+  // update input odom at scan
+  input_odom_at_scan_.header = odom->header;
+  input_odom_at_scan_.child_frame_id = odom->child_frame_id;
+  input_odom_at_scan_.pose = odom->pose;
+  input_odom_at_scan_.twist = odom->twist;
+}
+void CommunicationInterface::processScan()
+{
+  float angle_min = input_scan_.angle_min;
+  float angle_max = input_scan_.angle_max;
+  float angle_increment = input_scan_.angle_increment;
+  float range_min = input_scan_.range_min;
+  float range_max = input_scan_.range_max;
+  int number_of_laser = input_scan_.ranges.size();
   // get laser scan id of the mid +- 10 deg
   float priority_angle_range_deg = 10;
   float priority_angle_range_rad = priority_angle_range_deg * M_PI / 180.0;
@@ -40,11 +66,11 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
   // 4: angle max
 
   // process scan
-  sensor_msgs::PointCloud point_cloud;
+  filtered_point_cloud_.points.clear();
   static int frame_id_ = 0;
-  point_cloud.header.frame_id = "/base_footprint";
-  point_cloud.header.stamp = ros::Time::now();
-  point_cloud.header.seq = frame_id_;
+  filtered_point_cloud_.header.frame_id = "/base_footprint";
+  filtered_point_cloud_.header.stamp = ros::Time::now();
+  filtered_point_cloud_.header.seq = frame_id_;
   frame_id_++;
 
   std::pair<float, float> next_pos(0, 0);
@@ -52,27 +78,27 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
 
   curr_scan_.clear();
   int counter = 0;
-  int resolution = 10;  // pick only every ten points outside the interested area
+  int resolution = 10; // pick only every ten points outside the interested area
   for (int i = 0; i < number_of_laser; i++)
   {
     float laser_angle = angle_min + i * angle_increment;
     // robot heading: x axis, robot left: y axis
-    float x = scan->ranges[i] * std::cos(laser_angle);
-    float y = scan->ranges[i] * std::sin(laser_angle);
+    float x = input_scan_.ranges[i] * std::cos(laser_angle);
+    float y = input_scan_.ranges[i] * std::sin(laser_angle);
     // insert
-    double pos_x = robot_pose_.position.x;
-    double pos_y = robot_pose_.position.y;
-    tf::Quaternion q(robot_pose_.orientation.x, robot_pose_.orientation.y, robot_pose_.orientation.z,
-                     robot_pose_.orientation.w);
+    double pos_x = input_odom_at_scan_.pose.pose.position.x;
+    double pos_y = input_odom_at_scan_.pose.pose.position.y;
+    tf::Quaternion q(input_odom_at_scan_.pose.pose.orientation.x, input_odom_at_scan_.pose.pose.orientation.y,
+                     input_odom_at_scan_.pose.pose.orientation.z, input_odom_at_scan_.pose.pose.orientation.w);
     tf::Matrix3x3 m(q);
     double roll, pitch, yaw;
     m.getRPY(roll, pitch, yaw);
     double robot_heading = yaw;
     if (laser_angle > -priority_angle_range_rad && laser_angle < priority_angle_range_rad)
     {
-      if (std::isnan(scan->ranges[i]))  // if a point is nan, then the 10
-                                        // points before and after must be nan
-                                        // so that it can be considered empty
+      if (std::isnan(input_scan_.ranges[i])) // if a point is nan, then the 10
+                                             // points before and after must be nan
+                                             // so that it can be considered empty
       {
         // check the +- 10 points
         if ((priority_angle_range_rad - abs(laser_angle)) / angle_increment < 10)
@@ -84,7 +110,7 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
           bool valid = true;
           for (int j = 0; j < 10; j++)
           {
-            if (!std::isnan(scan->ranges[i + j]) || !std::isnan(scan->ranges[i - j]))
+            if (!std::isnan(input_scan_.ranges[i + j]) || !std::isnan(input_scan_.ranges[i - j]))
             {
               valid = false;
               break;
@@ -100,12 +126,12 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
         float global_x = x * std::cos(yaw) - y * std::sin(yaw) + pos_x;
         float global_y = x * std::sin(yaw) + y * std::cos(yaw) + pos_y;
 
-        curr_scan_[{ global_x, global_y }] = -20;
+        curr_scan_[{global_x, global_y}] = -20;
         if (range_max > max_dist)
         {
           next_pos.first = range_max * std::cos(laser_angle);
           next_pos.second = range_max * std::sin(laser_angle);
-          max_dist = scan->range_max;
+          max_dist = input_scan_.range_max;
         }
         continue;
       }
@@ -116,19 +142,19 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
       float global_y = x * std::sin(yaw) + y * std::cos(yaw) + pos_y;
       if (!std::isnan(global_x) && !std::isnan(global_y))
       {
-        curr_scan_[{ global_x, global_y }] = 20;
+        curr_scan_[{global_x, global_y}] = 20;
       }
       geometry_msgs::Point32 pt;
       pt.x = x;
       pt.y = y;
       pt.z = 0;
 
-      point_cloud.points.push_back(pt);
-      if (scan->ranges[i] > max_dist)
+      filtered_point_cloud_.points.push_back(pt);
+      if (input_scan_.ranges[i] > max_dist)
       {
         next_pos.first = x;
         next_pos.second = y;
-        max_dist = scan->ranges[i];
+        max_dist = input_scan_.ranges[i];
       }
     }
     else
@@ -140,14 +166,14 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
       }
       counter = 0;
       // insert
-      if (std::isnan(scan->ranges[i]))
+      if (std::isnan(input_scan_.ranges[i]))
       {
         float x = range_max * std::cos(laser_angle);
         float y = range_max * std::sin(laser_angle);
         float global_x = x * std::cos(yaw) - y * std::sin(yaw) + pos_x;
         float global_y = x * std::sin(yaw) + y * std::cos(yaw) + pos_y;
 
-        curr_scan_[{ global_x, global_y }] = 6;
+        curr_scan_[{global_x, global_y}] = 6;
 
         continue;
       }
@@ -155,36 +181,43 @@ void CommunicationInterface::scanOdomCallback(const sensor_msgs::LaserScan::Cons
       float global_y = x * std::sin(yaw) + y * std::cos(yaw) + pos_y;
       if (!std::isnan(global_x) && !std::isnan(global_y))
       {
-        curr_scan_[{ global_x, global_y }] = 6;
+        curr_scan_[{global_x, global_y}] = 6;
       }
       geometry_msgs::Point32 pt;
       pt.x = x;
       pt.y = y;
       pt.z = 0;
 
-      point_cloud.points.push_back(pt);
+      filtered_point_cloud_.points.push_back(pt);
     }
   }
   if (reached_pos_)
   {
     estimated_pos_ = next_pos;
-    // std::cerr << "scan received" << std::endl;
-    // std::cerr << curr_robot_pos_.first << " " << curr_robot_pos_.second << " " << estimated_pos_.first << " "
-    //           << estimated_pos_.second << std::endl;
   }
-  scan_publisher_.publish(point_cloud);
 }
 
-void CommunicationInterface::robotPositionCallback(const nav_msgs::Odometry msg)
+void CommunicationInterface::publishPointCloud()
+{
+  scan_publisher_.publish(filtered_point_cloud_);
+}
+void CommunicationInterface::robotPositionCallback(const nav_msgs::Odometry::Ptr &odom)
+{
+  input_odom_.header = odom->header;
+  input_odom_.child_frame_id = odom->child_frame_id;
+  input_odom_.pose = odom->pose;
+  input_odom_.twist = odom->twist;
+}
+
+void CommunicationInterface::processOdom()
 {
   PIDController pid_angle(1, 0.2, 2.5);
   PIDController pid_speed(0.1, 0.00, 0.0);
 
   geometry_msgs::Pose pose;
   geometry_msgs::Twist twist;
-  pose = msg.pose.pose;
-  robot_pose_ = pose;
-  twist = msg.twist.twist;
+  pose = input_odom_.pose.pose;
+  twist = input_odom_.twist.twist;
   double pos_x = pose.position.x;
   double pos_y = pose.position.y;
   tf::Quaternion q(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w);
@@ -251,20 +284,19 @@ void CommunicationInterface::robotPositionCallback(const nav_msgs::Odometry msg)
     set_speed = std::max(speed, -0.1);
   }
 
-  geometry_msgs::Twist new_twist;
   if (angle_setted)
   {
-    new_twist.linear.x = set_speed;
+    output_twist_.linear.x = set_speed;
   }
   else
   {
-    new_twist.linear.x = 0;
+    output_twist_.linear.x = 0;
   }
-  new_twist.linear.y = 0.0;
-  new_twist.linear.z = 0.0;
-  new_twist.angular.x = 0.0;
-  new_twist.angular.y = 0.0;
-  new_twist.angular.z = set_angle;
+  output_twist_.linear.y = 0.0;
+  output_twist_.linear.z = 0.0;
+  output_twist_.angular.x = 0.0;
+  output_twist_.angular.y = 0.0;
+  output_twist_.angular.z = set_angle;
 
   if (fabs(curr_robot_pos_.first - goal_.first) > 1e-1 || fabs(curr_robot_pos_.first - goal_.first) > 1e-1)
   {
@@ -272,7 +304,6 @@ void CommunicationInterface::robotPositionCallback(const nav_msgs::Odometry msg)
     // std::cerr << "curr pos: " << curr_robot_pos_.first << " " << curr_robot_pos_.second << " "
     //           << "next pos: " << goal_.first << " " << goal_.second << std::endl;
     reached_pos_ = false;
-    control_publisher_.publish(new_twist);
   }
   else
   {
@@ -281,21 +312,27 @@ void CommunicationInterface::robotPositionCallback(const nav_msgs::Odometry msg)
     //           << "next pos: " << goal_.first << " " << goal_.second << std::endl;
     reached_pos_ = true;
 
-    geometry_msgs::Twist stand_still;
+    output_twist_.linear.x = 0;
 
-    stand_still.linear.x = 0;
-
-    stand_still.linear.y = 0.0;
-    stand_still.linear.z = 0.0;
-    stand_still.angular.x = 0.0;
-    stand_still.angular.y = 0.0;
-    stand_still.angular.z = 0;
-    control_publisher_.publish(stand_still);
+    output_twist_.linear.y = 0.0;
+    output_twist_.linear.z = 0.0;
+    output_twist_.angular.x = 0.0;
+    output_twist_.angular.y = 0.0;
+    output_twist_.angular.z = 0;
   }
 }
 
+void CommunicationInterface::publishTwist()
+{
+  control_publisher_.publish(output_twist_);
+}
 void CommunicationInterface::cycle(Map &map)
 {
+  processScan();
+  processOdom();
+  publishTwist();
+  publishPointCloud();
+
   for (auto point : curr_scan_)
   {
     map.UpdateWithScanPoint(curr_robot_pos_.first, curr_robot_pos_.second, point.first.first, point.first.second,
