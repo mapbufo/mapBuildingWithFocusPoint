989da59f5a61febd77e64c3ff3adbacab6098738
yiluo <yi.luo@in-tech.com>
2020-07-31 14:14:22 +0200

    only empty points could be chosen for local path-planning

--- src/mapbufo/include/mapbufo/path_planning.h
+++ src/mapbufo/include/mapbufo/path_planning.h
@@ -3,25 +3,26 @@
 
 #include <map.h>
 #include <math.h>
-namespace PathPlanning {
-struct Point2DWithFloat {
-  Point2D point;
-  float value;
-};
+namespace PathPlanning
+{
+  struct Point2DWithFloat
+  {
+    Point2D point;
+    float value;
+  };
 
-std::vector<Point2D> GetNeighbors(Point2D current, Map map);
+  std::vector<Point2D> GetNeighbors(Point2D current, Map map, bool global);
 
-float CalculateCostSoFar(Point2D start_pos, Point2D current);
+  float CalculateCostSoFar(Point2D start_pos, Point2D current);
 
-float HeuristicFunction(Point2D next, Point2D end_pos);
+  float HeuristicFunction(Point2D next, Point2D end_pos);
 
-void OptimizePath(std::vector<Point2D> &path, Point2D start_pos);
+  void OptimizePath(std::vector<Point2D> &path, Point2D start_pos);
 
+  //TODO(YiLuo) : the path in the empty-area has higher priority than in the
+  // grey-area
+  std::vector<Point2D> PathPlanning(Point2D start_pos, Point2D end_pos,
+                                    const Map &map, bool global);
+} // namespace PathPlanning
 
-//TODO(YiLuo) : the path in the empty-area has higher priority than in the
-// grey-area
-std::vector<Point2D> PathPlanning(Point2D start_pos, Point2D end_pos,
-                                  const Map &map);
-}  // namespace PathPlanning
-
-#endif  // !PATH_PLANNING_H
+#endif // !PATH_PLANNING_H
